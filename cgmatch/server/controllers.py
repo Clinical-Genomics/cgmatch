57b34a1ca91b95da11d1cb705fb102c590db35cb
northwestwitch <rasi.chiara@gmail.com>
2018-11-12 20:25:56 +0100

    fixes custom request to reference server

--- cgmatch/server/controllers.py
+++ cgmatch/server/controllers.py
@@ -1,6 +1,9 @@
 import logging
+import json
 from flask import current_app
 from flask_pymongo import PyMongo
+from werkzeug.datastructures import Headers
+import requests
 
 LOG = logging.getLogger(__name__)
 
@@ -24,4 +27,62 @@ def a_patient(patient_id=None):
     else:
         patient_obj = mongo.db.patient.find_one( {'_id': patient_id} )
 
+    # if a patient is retrieved than parse its features (used downstream for patient query)
+    if patient_obj:
+        for feature in patient_obj.get('features'):
+            feature['id'] = feature.get('_id')
+            feature.pop('_id')
+
     return patient_obj
+
+def format_query(form_fields):
+    """ Create a patient query from the fields returned from a patient search form
+
+        Args:
+            form_fields(dict): form fields dictionary
+
+        Returns:
+            patient_query(dict) : a patient query object
+    """
+
+    patient_query= {
+        "patient": {
+            "id": str(form_fields.get("patients_id")),
+            "contact" : {
+                "name": form_fields.get("contact_name"),
+                "href": "mailto:"+form_fields.get("contact_email")
+            },
+            "features": eval(form_fields.get("features")),
+            #"genomicFeatures": str(form_fields.get("genomic_features")),
+            #features" : [{"id":"HP:0000522"}],
+            "genomicFeatures" : [{"gene":{"id":"NGLY1"}}]
+        }
+    }
+    return patient_query
+
+def post_request(server_url,headers,query):
+    """ Posts a HTTP request to a server and returns its response
+
+    Args:
+        headers (werkzeug.datastructures.Headers)
+        server_url(str): url of the server
+
+    Returns:
+        json_response: a json-formatted server response
+    """
+    json_response = None
+
+    try:
+        server_return = requests.request(
+            method = 'POST',
+            url = server_url,
+            headers = headers,
+            data = json.dumps(query)
+        )
+        # get json response:
+        json_response = server_return.json()
+
+    except Exception as err:
+        json_response = err
+
+    return json_response
--- cgmatch/server/views.py
+++ cgmatch/server/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
+import json
 from flask import render_template, flash, current_app, request
-
+from werkzeug.datastructures import Headers
 from cgmatch import app
 from . import controllers
 
@@ -12,8 +13,20 @@ def index():
 
     data = {}
     if request.method == 'POST':
-        flash("POST!")
         # create patient query
+        patient_query = controllers.format_query(request.form)
+        data['patient_query'] = patient_query
+
+        # query reference server, if available
+        refserver_url = current_app.config.get('REFSERVER_URL')
+        if refserver_url:
+
+            headers = Headers()
+            headers = {'Content-Type': 'application/vnd.ga4gh.matchmaker.v1.0+json', 'Accept': 'application/vnd.ga4gh.matchmaker.v1.0+json', 'X-Auth-Token': request.form.get("token")}
+            refserver_response = controllers.post_request(refserver_url, headers, patient_query)
+            data['refserver_response']=refserver_response
+
+
 
     # If connection to db is present get a test patient to populate the patient's form
     if current_app.config.get('MONGO_URI'):
