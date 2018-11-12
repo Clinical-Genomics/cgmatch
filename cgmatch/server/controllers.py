e58432f46d292a1abd8b7881d6860c588972c693
northwestwitch <rasi.chiara@gmail.com>
2018-11-12 12:51:13 +0100

    added query from random patient and improved search template

--- cgmatch/server/controllers.py
+++ cgmatch/server/controllers.py
@@ -1,15 +1,27 @@
 import logging
+from flask import current_app
+from flask_pymongo import PyMongo
+
 LOG = logging.getLogger(__name__)
 
 
-def benchmark_patients(id=None):
-    """ Return one or more benchmark patients objects
+def a_patient(patient_id=None):
+    """ Gets a specific patient when an id is provided,
+        otherwise returns a random patient from the 50 benchmark patients
 
-    Args:
-        id(str): patient id
+        Args:
+            patient_id(str) : the _id of the patient in matchmaker mongodb database
 
-    Returns:
-        patient_objs(list): a list of benchmark patient objects
-
+        Returns:
+            patient_obj(dic) : a matchmaker patient object
     """
-    return "Ecco i benchmark patients!"
+    LOG.info("retrieving a random patient from db")
+
+    mongo = PyMongo(current_app)
+    patient_obj = None
+    if patient_id == None: # this function should return a test patient
+        patient_obj = mongo.db.patient.find_one( {'contact.institution' : "Children's Hospital of Eastern Ontario"} )
+    else:
+        patient_obj = mongo.db.patient.find_one( {'_id': patient_id} )
+
+    return patient_obj
--- cgmatch/server/views.py
+++ cgmatch/server/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-from flask import render_template, flash, request
+from flask import render_template, flash, current_app, request
 
 from cgmatch import app
 from . import controllers
@@ -11,15 +11,17 @@ LOG = logging.getLogger(__name__)
 def index():
 
     data = {}
-
     if request.method == 'POST':
-
         flash("POST!")
         # create patient query
 
-        #patient_query = controllers.create_query(request.form)
-    else:
-        benchmark_patients =  controllers.benchmark_patients()
-        data['benchmark_patients'] = benchmark_patients
+    # If connection to db is present get a test patient to populate the patient's form
+    if current_app.config.get('MONGO_URI'):
+        test_patient_obj = controllers.a_patient()
+
+        if test_patient_obj:
+            data['test_patient'] = test_patient_obj
+
+
 
     return render_template('search.html', **data)
